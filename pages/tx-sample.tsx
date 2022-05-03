4d11f72daf7bfcc802666c89c83b45b347c79379
zulianrizki <you@example.com>
2022-05-04 00:27:50 +0700

    fix: :bug:

--- pages/tx-sample.tsx
+++ pages/tx-sample.tsx
@@ -1,4 +1,4 @@
-import { Fee, MsgSend } from "@terra-money/terra.js";
+import { Fee, Msg, MsgSend } from "@terra-money/terra.js";
 import {
   CreateTxFailed,
   Timeout,
@@ -65,12 +65,64 @@ export default function TxSample() {
       });
   }, [connectedWallet]);
 
+  const post = useCallback(
+    (rawMsgs: Array<string>) => {
+      if (!connectedWallet) {
+        return;
+      }
+
+      if (connectedWallet.network.chainID.startsWith("columbus")) {
+        alert(`Please only execute this example on Testnet`);
+        return;
+      }
+
+      setTxResult(null);
+      setTxError(null);
+
+      connectedWallet
+        .post({
+          fee: new Fee(1000000, "200000uusd"),
+          msgs: rawMsgs.map((msg) => Msg.fromAmino(JSON.parse(msg))),
+        })
+        .then((nextTxResult: TxResult) => {
+          console.log(nextTxResult);
+          setTxResult(nextTxResult);
+        })
+        .catch((error: unknown) => {
+          if (error instanceof UserDenied) {
+            setTxError("User Denied");
+          } else if (error instanceof CreateTxFailed) {
+            setTxError("Create Tx Failed: " + error.message);
+          } else if (error instanceof TxFailed) {
+            setTxError("Tx Failed: " + error.message);
+          } else if (error instanceof Timeout) {
+            setTxError("Timeout");
+          } else if (error instanceof TxUnspecifiedError) {
+            setTxError("Unspecified Error: " + error.message);
+          } else {
+            setTxError(
+              "Unknown Error: " +
+                (error instanceof Error ? error.message : String(error))
+            );
+          }
+        });
+    },
+    [connectedWallet]
+  );
+
   useEffect(() => {
     eventEmitter.on("test", proceed);
     return () => {
       eventEmitter.off("test", proceed);
     };
   }, [proceed]);
+
+  useEffect(() => {
+    eventEmitter.on("post", post);
+    return () => {
+      eventEmitter.off("post", post);
+    };
+  }, [post]);
 
   return (
     <div>
